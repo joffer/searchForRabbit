6b9ccdab36ffedb4cc11082b12702a9730202c5d
joffer <greenjoffer@gmail.com>
2019-08-22 01:39:09 +0300

    added negative response branch

--- test_request.py
+++ test_request.py
@@ -2,15 +2,17 @@ import requests
 import json
 
 URL = 'http://127.0.0.1:5000/array/search/binary/'
-t_array = [1,3,9,5,8,10,12]
-elem_in_array = 10
+t_array = [1,3,5,8,10,12]
+elem_in_array = 5
 
 request_data = {
     'search_array': t_array, 
     'search_element': elem_in_array
 }
 post_data = json.dumps(request_data)
-
 resp = requests.post(URL, json=post_data)
+
 if resp.ok:
     print("Result:\n",resp.text, sep='')
+else:
+    print("Could not get result, instead got:\n",resp.text, sep='')
