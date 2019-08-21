f52e687142299edb5acf18ab91dda762e5325801
joffer <greenjoffer@gmail.com>
2019-08-22 02:55:53 +0300

    Changed negative response text

--- test_request.py
+++ test_request.py
@@ -3,7 +3,7 @@ import json
 
 URL = 'http://127.0.0.1:5000/array/search/binary/'
 t_array = [1,3,5,8,10,12]
-elem_in_array = 5
+elem_in_array = 8
 
 request_data = {
     'search_array': t_array, 
@@ -15,4 +15,4 @@ resp = requests.post(URL, json=post_data)
 if resp.ok:
     print("Result:\n",resp.text, sep='')
 else:
-    print("Could not get result, instead got:\n",resp.text, sep='')
+    print("Could not get result:\n",resp.text, sep='')
